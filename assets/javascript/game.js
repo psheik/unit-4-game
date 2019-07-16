01b1968dcb8be4e7f7d9e85e129c8ceb13321996
Pasha Sheik <pashasheik33@gmail.com>
2019-07-16 11:38:35 -0700

    initial upload of css js html

---
+++ assets/javascript/game.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+
+    // Game variables set
+    var randomNum = 0;
+    var score = 0;
+    var wins = 0;
+    var losses = 0;
+  
+    // Function generates random number
+    function generateNum(minNumber, maxNumber) {
+      return Math.floor(Math.random() * (maxNumber - minNumber) + minNumber);
+    };
+  
+    // Function sets crystal values
+    function setCrystalvalue(crystalType){
+      var crystalValue = generateNum(1, 12);
+      $("#" + crystalType).attr("data-value", crystalValue);
+    }
+  
+    // Function resets game
+    function newGame() {
+      randomNum = generateNum(19, 120);
+      $("#randomNum").text(randomNum);
+      $(".crystal").each(function(){
+        setCrystalvalue($(this).attr('id'));
+      })
+    }
+  
+    // Functions checks for game ending
+    function gameOver(){
+      if (score > randomNum){
+        losses++;
+        $("#losses").text(losses);
+        alert("You Lost!!");
+        score = 0;
+        newGame();
+        $("#score").text(score);
+      } else if (score == randomNum){
+        wins++;
+        $("#wins").text(wins);
+        alert("You won!!");
+        newGame();
+        score = 0;
+        $("#score").text(score);
+      }
+    }
+  
+    // Clicker function for crystals to increase score and verify whether game ended
+    $(".crystal").on("click", function(){
+      score = score + parseInt($(this).attr("data-value"));
+      $("#score").text(score);
+      gameOver();
+    });
+  
+    newGame();
+  });
